282d5f33bd4ff6611ed85504520d47b5b57d1fa9
Martin Lopez <martinald467@gmail.com>
2024-01-14 20:19:55 +0100

    add mem module

---
+++ mem/mem.go
@@ -0,0 +1,99 @@
+package mem
+
+import (
+	"strconv"
+	"strings"
+
+	"github.com/SirNoob97/server-monitor/utils"
+)
+
+func VirtualMemory() (*VirtualMemoryStat, error) {
+	location := utils.GetEnv("HOST_PROC", "/proc")
+	filename := utils.JoinPath(location, "meminfo")
+	lines, err := utils.ReadLines(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	memavail := false
+	ret := &VirtualMemoryStat{}
+
+	for _, ln := range lines {
+		fields := strings.Split(ln, ":")
+		if len(fields) != 2 {
+			continue
+		}
+		key := strings.TrimSpace(fields[0])
+		value := strings.TrimSpace(fields[1])
+		value = strings.Replace(value, " kB", "", -1)
+
+		switch key {
+		case "MemTotal":
+			t, err := strconv.ParseUint(value, 10, 64)
+			if err != nil {
+				return ret, err
+			}
+			ret.Total = t
+		case "MemFree":
+			t, err := strconv.ParseUint(value, 10, 64)
+			if err != nil {
+				return ret, err
+			}
+			ret.Free = t
+		case "MemAvailable":
+			t, err := strconv.ParseUint(value, 10, 64)
+			if err != nil {
+				return ret, err
+			}
+			memavail = true
+			ret.Available = t
+		case "Buffers":
+			t, err := strconv.ParseUint(value, 10, 64)
+			if err != nil {
+				return ret, err
+			}
+			ret.Buffers = t
+		case "Cached":
+			t, err := strconv.ParseUint(value, 10, 64)
+			if err != nil {
+				return ret, err
+			}
+			ret.Cached = t
+		case "SReclaimable":
+			t, err := strconv.ParseUint(value, 10, 64)
+			if err != nil {
+				return ret, err
+			}
+			ret.Sreclaimable = t
+		case "SwapCached":
+			t, err := strconv.ParseUint(value, 10, 64)
+			if err != nil {
+				return ret, err
+			}
+			ret.SwapCached = t
+		case "SwapTotal":
+			t, err := strconv.ParseUint(value, 10, 64)
+			if err != nil {
+				return ret, err
+			}
+			ret.SwapTotal = t
+		case "SwapFree":
+			t, err := strconv.ParseUint(value, 10, 64)
+			if err != nil {
+				return ret, err
+			}
+			ret.SwapFree = t
+		}
+	}
+
+	ret.Cached += ret.Sreclaimable
+
+	if !memavail {
+		ret.Available = ret.Cached + ret.Free
+	}
+
+	ret.Used = ret.Total - ret.Free - ret.Buffers - ret.Cached
+	ret.UsedPercent = float64(ret.Used) / float64(ret.Total) * 100.0
+
+	return ret, nil
+}
---
+++ mem/mem_structs.go
@@ -0,0 +1,44 @@
+package mem
+
+type VirtualMemoryStat struct {
+	// Total amount of RAM
+	Total uint64 `json:"total"`
+
+	// RAM available for programs to allocate
+	Available uint64 `json:"available"`
+
+	// RAM used by programs
+	Used uint64 `json:"used"`
+
+	// Percentage of RAM used by programs
+	UsedPercent float64 `json:"usedPercent"`
+
+	// This is the kernel's notion of free memory; RAM chips whose bits nobody
+	// cares about the value of right now. For a human consumable number,
+	// Available is what you really want.
+	Free uint64 `json:"free"`
+
+	// Linux values
+	// https://www.kernel.org/doc/Documentation/filesystems/proc.txt
+
+	// Relatively temporary storage for raw disk blocks
+	Buffers uint64 `json:"buffers"`
+
+	// In-memory cache for files read from the disk (the pagecache).
+	// Doesn't include SwapCached
+	Cached uint64 `json:"cached"`
+
+	// Part of Slab, that might be reclaimed, such as caches
+	Sreclaimable uint64 `json:"sreclaimable"`
+
+	// Memory that once was swapped out, is swapped back in but still also is in the swapfile
+	// if memory is needed it doesn't need to be swapped out AGAIN because it is already in the swapfile.
+	// This saves I/O
+	SwapCached uint64 `json:"swapCached"`
+
+	// Total amount of swap space available
+	SwapTotal uint64 `json:"swapTotal"`
+
+	// Memory which has been evicted from RAM, and is temporarily on the disk
+	SwapFree uint64 `json:"swapFree"`
+}
