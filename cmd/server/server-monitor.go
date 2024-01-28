7255c38dafd4cfcfda24268efda9570a89ce7537
Martin Lopez <martinald467@gmail.com>
2024-01-28 17:30:20 +0100

    replace http server form standard library with gorilla mux http server

--- cmd/server/server-monitor.go
+++ cmd/server/server-monitor.go
@@ -1,14 +1,48 @@
 package main
 
 import (
+	"context"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/SirNoob97/server-monitor/server/handler"
+	"github.com/SirNoob97/server-monitor/server/middleware"
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	mux := http.NewServeMux()
-	mux.Handle("/cpu", &handler.Cpu{})
+	router := mux.NewRouter()
+	sr := router.PathPrefix("/server-monitor").Methods(http.MethodGet).Subrouter()
+	sr.HandleFunc("/cpu", handler.GetCPUInfo)
+	sr.Use(middleware.Logging)
+	sr.Use(middleware.DenyGetRequestsBody)
 
-	http.ListenAndServe(":8080", mux)
+	server := http.Server{
+		Addr:         "0.0.0.0:8080",
+		WriteTimeout: time.Second * 2,
+		ReadTimeout:  time.Second * 5,
+		IdleTimeout:  time.Second * 10,
+		Handler:      sr,
+	}
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	signalChann := make(chan os.Signal, 1)
+	signal.Notify(signalChann, os.Interrupt)
+	<-signalChann
+
+	timeout := time.Second * 15
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	server.Shutdown(ctx)
+	log.Println("Server shutting down")
+	os.Exit(0)
 }
